e78db1a7ecc4a922ebd3277d93679e929f82dcf6
deeptadevkota <deeptadevkota21@gmail.com>
2021-10-18 18:31:51 +0545

    dummy-data

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -11,12 +11,39 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SfCalendar(
-          view: CalendarView.month,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+      ),
+      body: SfCalendar(
+        view: CalendarView.week,
+        dataSource: MeetingDataSource(getAppointments()),
       ),
     );
+  }
+}
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: 'Testing',
+      color: Colors.blue));
+
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
